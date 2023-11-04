455e749d11664859b48e90168936599f90262f26
CubeConvict <kraft.toby@gmail.com>
2023-11-03 17:17:07 -0700

    Update array_diff.py

--- array_diff.py
+++ array_diff.py
@@ -11,27 +11,6 @@ If a value is present in b, all of its occurrences must be removed from the othe
 array_diff([1,2,2,2,3],[2]) == [1,3]
 '''
 
-
-def array_diff(original_array,query_array):
-    #iterate through list one 
-    #print("Original array:",len(original_array))
-    #popping shortens array
-    new_array = original_array.copy()
-    for x in original_array:
-        #check to see if is in list 2 by iterating through list 2 with equality
-        for y in query_array:
-            #if it is in there, pop it
-            if x == y:
-                    print("original ",x,"query ",y,"matches: ", x == y)
-                    new_array.pop(original_array.index(x))
-            else:
-                print("original ",x,"query ",y,"matches: ", x == y) 
-            
-
-    return new_array 
-    
-
-
 test_array1 = [1,2]
 test_array2 = [1,2,2]
 test_array3 = [1,2,2,2,3]
@@ -41,8 +20,28 @@ pop1 = [1]
 pop2 = [2]
 pop_array = [1, 2]
 
-#print(array_diff([1,2,3],[1,2]) == [3])
+def array_diff(original_array,query_array):
+    #initiate an empty array
+    new_array = []
+    i = 0
+    if query_array == [] or original_array == []:
+        return original_array
+    else:
+        for y in query_array:
+             #check to see if is in list 2 by iterating through list 2 with equality          
+            for x in original_array:
+                #if it is in there, pop it
+                # cant use if x != y because you'll 
+                if x not in query_array and y not in new_array:
+                    new_array.append(x)
 
-#print(test_array4)
-print(array_diff(test_array4,pop_array))
+    return new_array 
+    
+
+print(array_diff(test_array1,pop1), ' should be [2]')
+print(array_diff(test_array2,pop1), " expected [2,2]")
+print(array_diff(test_array2,pop2), " expected [1]")
+print(array_diff(test_array2,[]), " expected [1,2,2]")
+print(array_diff([],test_array1), " expected []")
+print(array_diff(test_array4,pop_array), " expected [3]")
 
