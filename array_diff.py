9d28a9707ba0b1a8b2b2e09f0e8442a944770324
MJ <70070416+j5ndev@users.noreply.github.com>
2023-11-09 08:52:02 -0500

    First stab at array_diff

--- array_diff.py
+++ array_diff.py
@@ -11,6 +11,22 @@ If a value is present in b, all of its occurrences must be removed from the othe
 array_diff([1,2,2,2,3],[2]) == [1,3]
 '''
 
+def array_diff(original_array, query_array):
+    '''Remove all values from original_arry, which are present in query_array, keeping their order'''
+
+    # Exit early if either array is empty
+    if not original_array or not query_array:
+        return original_array
+
+    # Build a new_array with original_array values not present in query_array
+    new_array = []
+    for x in query_array:
+        if x not in query_array:
+            new_array.append(x)
+
+    return new_array 
+
+
 test_array1 = [1,2]
 test_array2 = [1,2,2]
 test_array3 = [1,2,2,2,3]
@@ -20,28 +36,10 @@ pop1 = [1]
 pop2 = [2]
 pop_array = [1, 2]
 
-def array_diff(original_array,query_array):
-    #initiate an empty array
-    new_array = []
-    i = 0
-    if query_array == [] or original_array == []:
-        return original_array
-    else:
-        for y in query_array:
-             #check to see if is in list 2 by iterating through list 2 with equality          
-            for x in original_array:
-                #if it is in there, pop it
-                # cant use if x != y because you'll 
-                if x not in query_array and y not in new_array:
-                    new_array.append(x)
 
-    return new_array 
-    
-
-print(array_diff(test_array1,pop1), ' should be [2]')
-print(array_diff(test_array2,pop1), " expected [2,2]")
-print(array_diff(test_array2,pop2), " expected [1]")
-print(array_diff(test_array2,[]), " expected [1,2,2]")
-print(array_diff([],test_array1), " expected []")
-print(array_diff(test_array4,pop_array), " expected [3]")
-
+assert(array_diff(test_array1, pop1) == [2])
+assert(array_diff(test_array2, pop1) == [2,2])
+assert(array_diff(test_array2, pop2) == [1])
+assert(array_diff(test_array2, []) == [1,2,2])
+assert(array_diff([], test_array1) == [])
+assert(array_diff(test_array4, pop_array) == [3])
